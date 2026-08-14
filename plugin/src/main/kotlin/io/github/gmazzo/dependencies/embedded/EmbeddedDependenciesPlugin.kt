426aa1928e2b52096b362fbb7b7b8e0eec141a70
Guillermo Mazzola <gmazzo65@gmail.com>
2026-08-14 13:08:07 +0200

    Fixed Gradle build chain dependencies inferring

--- plugin/src/main/kotlin/io/github/gmazzo/dependencies/embedded/EmbeddedDependenciesPlugin.kt
+++ plugin/src/main/kotlin/io/github/gmazzo/dependencies/embedded/EmbeddedDependenciesPlugin.kt
@@ -5,19 +5,19 @@ import org.gradle.api.Project
 import org.gradle.api.attributes.Category.CATEGORY_ATTRIBUTE
 import org.gradle.api.attributes.Category.LIBRARY
 import org.gradle.api.attributes.LibraryElements
+import org.gradle.api.attributes.LibraryElements.CLASSES
 import org.gradle.api.attributes.LibraryElements.JAR
 import org.gradle.api.attributes.LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE
+import org.gradle.api.attributes.LibraryElements.RESOURCES
 import org.gradle.api.file.ConfigurableFileCollection
-import org.gradle.api.file.DuplicatesStrategy
 import org.gradle.api.plugins.ExtensionAware
+import org.gradle.api.tasks.AbstractCopyTask
 import org.gradle.api.tasks.SourceSet.MAIN_SOURCE_SET_NAME
 import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.Sync
 import org.gradle.kotlin.dsl.add
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.domainObjectContainer
 import org.gradle.kotlin.dsl.named
-import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.registerTransform
 import org.gradle.kotlin.dsl.the
 
@@ -58,12 +58,22 @@ public class EmbeddedDependenciesPlugin : Plugin<Project> {
             val spec = specs.maybeCreate(name)
 
             val discriminator = if (name == MAIN_SOURCE_SET_NAME) "embedded" else "embedded-$name"
-            val jarElements: LibraryElements = objects.named(JAR)
-            val repackagedJarElements: LibraryElements = objects.named("$JAR+repackaged+$discriminator")
+            val repackagedClassesElements: LibraryElements = objects.named("$CLASSES+repackaged+$discriminator")
+            val repackagedResourcesElements: LibraryElements = objects.named("$RESOURCES+repackaged+$discriminator")
 
             dependencies.registerTransform(EmbeddedDependenciesTransform::class) {
-                from.attributes.attribute(LIBRARY_ELEMENTS_ATTRIBUTE, jarElements)
-                to.attributes.attribute(LIBRARY_ELEMENTS_ATTRIBUTE, repackagedJarElements)
+                from.attributes.attribute(LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(JAR))
+                to.attributes.attribute(LIBRARY_ELEMENTS_ATTRIBUTE, repackagedClassesElements)
+                parameters.forResources.value(false)
+                parameters.includes.value(spec.includes)
+                parameters.excludes.value(spec.excludes)
+                parameters.mappings.value(spec.repackages)
+            }
+
+            dependencies.registerTransform(EmbeddedDependenciesTransform::class) {
+                from.attributes.attribute(LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(JAR))
+                to.attributes.attribute(LIBRARY_ELEMENTS_ATTRIBUTE, repackagedResourcesElements)
+                parameters.forResources.value(true)
                 parameters.includes.value(spec.includes)
                 parameters.excludes.value(spec.excludes)
                 parameters.mappings.value(spec.repackages)
@@ -73,10 +83,6 @@ public class EmbeddedDependenciesPlugin : Plugin<Project> {
                 configurations.create(if (name == MAIN_SOURCE_SET_NAME) "embedded" else "${name}Embedded") config@{
                     isCanBeResolved = true
                     isCanBeConsumed = false
-                    attributes {
-                        attribute(CATEGORY_ATTRIBUTE, objects.named(LIBRARY))
-                        attribute(LIBRARY_ELEMENTS_ATTRIBUTE, jarElements)
-                    }
                     (this@config as ExtensionAware).extensions.add(
                         publicType = EmbeddedDependenciesSpec::class,
                         name = "embedding",
@@ -84,28 +90,29 @@ public class EmbeddedDependenciesPlugin : Plugin<Project> {
                     )
                 }
 
-            val transformedJars = config.incoming
-                .artifactView { attributes.attribute(LIBRARY_ELEMENTS_ATTRIBUTE, repackagedJarElements) }
+            val embeddedClasses = config.incoming
+                .artifactView {
+                    attributes {
+                        attribute(CATEGORY_ATTRIBUTE, objects.named(LIBRARY))
+                        attribute(LIBRARY_ELEMENTS_ATTRIBUTE, repackagedClassesElements)
+                    }
+                }
                 .files
 
-            val transformedFiles = files(transformedJars.elements.map { it.map(::zipTree) })
-                .apply { finalizeValueOnRead() }
-
-            fun extractTask(resources: Boolean) =
-                tasks.register<Sync>("extract${if (spec.name == MAIN_SOURCE_SET_NAME) "" else spec.name.replaceFirstChar { it.uppercase() }}EmbeddedDependencies${if (resources) "Resources" else "Classes"}") {
-                    from(transformedFiles)
-                    into(layout.buildDirectory.dir("embedded-classes/${spec.name}/${if (resources) "resources" else "classes"}"))
-                    (if (resources) exclude("**/*.class") else include("**/*.class"))
-                    includeEmptyDirs = false
-                    duplicatesStrategy = DuplicatesStrategy.WARN
+            val embeddedResources = config.incoming
+                .artifactView {
+                    attributes {
+                        attribute(CATEGORY_ATTRIBUTE, objects.named(LIBRARY))
+                        attribute(LIBRARY_ELEMENTS_ATTRIBUTE, repackagedResourcesElements)
+                    }
                 }
+                .files
 
-            val extractClasses = extractTask(resources = false)
-            val extractResources = extractTask(resources = true)
-
-            dependencies.add(compileOnlyConfigurationName, transformedJars)
-            (output.classesDirs as ConfigurableFileCollection).from(extractClasses)
-            resources.srcDir(extractResources)
+            dependencies.add(compileOnlyConfigurationName, embeddedClasses)
+            (output.classesDirs as ConfigurableFileCollection).from(embeddedClasses)
+            tasks.named<AbstractCopyTask>(processResourcesTaskName) {
+                from(embeddedResources)
+            }
         }
 
     }
